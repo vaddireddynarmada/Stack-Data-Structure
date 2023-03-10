136d7f7c23ba3c12775cbb3c12291ec8dcae9a8e
unknown <narmada.vaddireddy97@gmail.com>
2023-03-10 20:50:15 +0530

    Added: Code to print adequeue beginning using Linked List

--- Queue.java
+++ Queue.java
@@ -1,24 +1,49 @@
-public class Queue<K>{
+public class Queue<K> {
     private final LinkedList queueLinkedList;
+
     public Queue() {
         queueLinkedList = new LinkedList();
-        }
-        public void enqueue(INode element) {
-        queueLinkedList.append(element);
-        }
+    }
 
-        public void printQueue() {
-        queueLinkedList.printLinkedList();
-        }
-        public static void main(String[] args) {
-            Queue QueueList = new Queue();
-            MyNode<Integer> firstNode = new MyNode<Integer>(56);
-            MyNode<Integer> secondNode = new MyNode<Integer>(30);
-            MyNode<Integer> thirdNode = new MyNode<Integer>(70);
-            QueueList.enqueue(firstNode);
-            QueueList.enqueue(secondNode);
-            QueueList.enqueue(thirdNode);
-            QueueList.printQueue();
+    public void enqueue(INode element) {
+        queueLinkedList.append(element);
+    }
+
+    public INode dequeue() {
+        return queueLinkedList.pop();
+    }
+
+    public boolean isEmpty() {
+        if (queueLinkedList.head != null) {
+            return false;
+        } else {
+            return true;
         }
     }
+
+    public int size() {
+        return queueLinkedList.size();
+    }
+
+    public void printQueue() {
+        queueLinkedList.printLinkedList();
+    }
+
+    public static void main(String[] args) {
+        MyNode<Integer> firstNode = new MyNode<Integer>(56);
+        MyNode<Integer> secondNode = new MyNode<Integer>(30);
+        MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+        Queue queueList = new Queue();
+        queueList.enqueue(firstNode);
+        queueList.enqueue(secondNode);
+        queueList.enqueue(thirdNode);
+        queueList.printQueue();
+        System.out.println("Size of the Queue is: " + queueList.size());
+        System.out.println("Queue after repeated dequeue : ");
+        while (!queueList.isEmpty()) {
+            queueList.dequeue();
+            queueList.printQueue();
+        }
+    }
+}
 
