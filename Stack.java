5d92b566489f5b11e043f317715e70e8ade534da
unknown <narmada.vaddireddy97@gmail.com>
2023-03-10 20:35:17 +0530

    Added: Code to print peak and pop from the Stack till it is empty

--- Stack.java
+++ Stack.java
@@ -9,18 +9,46 @@ public class Stack<K> {
         stackLinkedList.add(element);
     }
 
+    public INode peek() {
+        return stackLinkedList.head;
+    }
+
+    public INode pop() {
+
+        return stackLinkedList.pop();
+    }
+
+    public boolean isEmpty() {
+        if (stackLinkedList.head != null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public int size() {
+        return stackLinkedList.size();
+    }
+
     public void printStack() {
         stackLinkedList.printLinkedList();
     }
 
     public static void main(String[] args) {
-        Stack stackList = new Stack();
         MyNode<Integer> firstNode = new MyNode<Integer>(70);
         MyNode<Integer> secondNode = new MyNode<Integer>(30);
         MyNode<Integer> thirdNode = new MyNode<Integer>(56);
+        Stack stackList = new Stack();
         stackList.push(firstNode);
         stackList.push(secondNode);
         stackList.push(thirdNode);
+        stackList.printStack();
+        System.out.println("Top element of stack: " + stackList.peek().getKey());
+        System.out.println("Size of the stack is: " + stackList.size());
+        System.out.println("Stack after repeated popping : ");
+        while (!stackList.isEmpty()) {
+            stackList.pop();
+        }
         stackList.printStack();
     }
 }
